f16aabcd87012d7ef681a488323838748e5160d9
quijano_flores <quijano@braingain.co>
2016-10-12 17:22:27 +0530

    added global variables

--- src/main/java/sparkexample/Hello.java
+++ src/main/java/sparkexample/Hello.java
@@ -15,6 +15,7 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.sentdetect.SentenceModel;
@@ -23,6 +24,7 @@ import opennlp.tools.sentdetect.SentenceDetectorME;
 public class Hello {
 
     static SentenceModel model;
+    static InputStream modelIn;
 
     public static void main(String[] args) {
         final Configuration configuration = new Configuration();
@@ -48,52 +50,55 @@ public class Hello {
         post("/sait", (request, response) -> {
             System.out.println("main.post./sayit");
             StringWriter writer = new StringWriter();
+            //get the models from source, used to detect sentences
             try {
                 System.out.println("trying to get the InputStream");
-                InputStream modelIn = new FileInputStream("src/resources/input/en-sent.bin");
+                modelIn = new FileInputStream("src/resources/input/en-sent.bin");
                 System.out.println(modelIn);
-                try {
-                    model = new SentenceModel(modelIn);
-                    System.out.println(model);
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-                finally {
-                    if (modelIn != null) {
-                        try {
-                            modelIn.close();
-                        }
-                        catch (IOException e) {
-                        }
-                    }
-                }
-                //System.out.println(model);
+
             }
             catch (FileNotFoundException e) {
                 System.out.println("something went wrong" + e);
                 e.printStackTrace();
             }
-
-
-
+            //create a new model
+            try {
+                model = new SentenceModel(modelIn);
+                System.out.println(model);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if (modelIn != null) {
+                    try {
+                        modelIn.close();
+                    }
+                    catch (IOException e) {
+                    }
+                }
+            }
+            //from the user input, split the paragraph into sentences
             try {
                 String user_input_text = request.queryParams("user_input_text") != null ? request.queryParams("user_input_text") : "anonymous";
                 System.out.println(user_input_text);
-                //String email = request.queryParams("email") != null ? request.queryParams("email") : "unknown";
                 SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
      
-                String sentences[] = sentenceDetector.sentDetect(user_input_text);
-                System.out.println(Arrays.toString(sentences));
- 
+                String[] sentences = sentenceDetector.sentDetect(user_input_text);
+
+                System.out.println(sentences.getClass());
+                for (String sentence : sentences) {
+                    System.out.println(sentence);
+                }
+
                 Template resultTemplate = configuration.getTemplate("templates/result.ftl");
  
                 Map<String, Object> map = new HashMap<>();
                 map.put("user_input_text", user_input_text);
-                //map.put("email", email);
- 
+                map.put("sentences", sentences);
                 resultTemplate.process(map, writer);
             } catch (Exception e) {
+                System.out.println(e);
                 halt(500);
             }
             return writer;
